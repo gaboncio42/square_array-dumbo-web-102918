74a071a01aadd759055f3f98b1ef1aca4aadffc5
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-12 22:34:48 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
   new_squares = []
-  array.each {|num| Math.sqrt(num)}
-  new_squares.push(array)
-  return new_squares
+  array.each do |num|
+    new_squares.push(num * num)
+  end
+  new_squares
 end
