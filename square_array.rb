e75bcadeade13f2a276a0c1e3c598818f206731c
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-10-12 22:26:31 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,4 @@
 def square_array(array)
   new_squares = []
-  array.each do |num|
-    new_squares.push(num * num)
-  end
-  return new_squares
+  array.each {|num| new_squares.push(num * num)
 end
